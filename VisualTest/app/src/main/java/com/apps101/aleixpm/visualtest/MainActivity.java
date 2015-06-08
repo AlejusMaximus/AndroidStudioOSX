d28d078d77a7b44d70c9718ebbdaa18a14cb599d
AlejusMaximus <aleix.pascual@gmail.com>
2015-06-08 21:59:21 +0200

    commit .apk

--- VisualTest/app/src/main/java/com/apps101/aleixpm/visualtest/MainActivity.java
+++ VisualTest/app/src/main/java/com/apps101/aleixpm/visualtest/MainActivity.java
@@ -1,11 +1,15 @@
 package com.apps101.aleixpm.visualtest;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -18,24 +22,49 @@ import java.io.IOException;
 
 public class MainActivity extends ActionBarActivity {
 
+    // Debugging
+    private static final String TAG = "Visual_Engin";
+    private static final boolean D = true;
+
     TextView outputURLs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        outputURLs = (TextView) findViewById(R.id.outputURLs);
-        UnitTest();
+
+        //Connecting to the Network:
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            Toast.makeText(this, "Network available", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Network not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(D) Log.e(TAG, "++ ON START ++");
+        outputURLs = (TextView) findViewById(R.id.outputURLs);
+
+        UnitTest();
+
+    }
+
     /**
      * This method is used for unit testing
      * @author aleixpm
      * @version 1.0
-     * @param TBD
+     * @param TODO
      * @return true or false
      */
     public boolean UnitTest(){
-        Log.d("Jsoup","Inside UnitTest");
+        Log.d("Jsoup", "Inside UnitTest");
         //TODO:
         //http://jsoup.org/cookbook/extracting-data/dom-navigation
         //http://www.survivingwithandroid.com/2014/04/parsing-html-in-android-with-jsoup.html
@@ -59,6 +88,25 @@ public class MainActivity extends ActionBarActivity {
         return true;
 
     }
+
+    @Override
+    public synchronized void onPause() {
+        super.onPause();
+        if(D) Log.e(TAG, "- ON PAUSE -");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(D) Log.e(TAG, "-- ON STOP --");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(D) Log.e(TAG, "--- ON DESTROY ---");
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
